e63b4754f21b900d7270eb5334f18c2ebf42146a
0ClaytonBigsby0 <129800720+0ClaytonBigsby0@users.noreply.github.com>
2026-07-02 12:38:19 +0100

    Add root favicon redirect

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,6 @@
+from django.templatetags.static import static
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
@@ -6,6 +8,11 @@ app_name = "portfolio"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=static("portfolio/images/favicon.ico"), permanent=True),
+        name="favicon",
+    ),
     path("start-project/", views.start_project, name="start_project"),
     path("projects/<slug:slug>/", views.project_detail, name="project_detail")
 ]
